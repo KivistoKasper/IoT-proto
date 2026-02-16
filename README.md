0d74a3212b99e68b6d4fbf572385eb54fc64d1fb
Kivistö Kasper <kivisto.kasper@gmail.com>
2026-02-16 13:56:56 +0200

    Adding some comments and updating readme

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ COMP.CE.450-2026 Internet of Things
 ## Dependecies
 - Seeed HT02 'https://github.com/Seeed-Studio/Grove_Temper_Humidity' 
 
+
+## Run this in local linux
+```
+g++ miner.cpp -o miner.out
+./miner.out 
+Tidity: 28.56 %
+Temperature: 22.06 °C  Humidity: 28.75 %
+Temperature: 22.00 °C  Humidity: 28.75 %
+Temperature: 22.06 °C  Humidity: 28.87 %
+^C
+```
--- src/miner.cpp
+++ src/miner.cpp
@@ -6,31 +6,36 @@
 
 int main() {
     const char* port = "/dev/ttyACM0";   // Port where the Arduino is connected
-    int serial_port = open(port, O_RDONLY);
+    int serial_port = open(port, O_RDONLY); // Open the serial port for reading only
 
+    // Check if the port was opened successfully
     if(serial_port < 0) {
         std::cerr << "Error opening serial port\n";
         return 1;
     }
 
+    // configure strcture that contains the settings for the serial port
     struct termios tty;
     memset(&tty, 0, sizeof tty);
 
+    // Get the current configuration of the serial port
     if(tcgetattr(serial_port, &tty) != 0) {
         std::cerr << "Error from tcgetattr\n";
         return 1;
     }
 
+    // Set the baud rate to 9600
     cfsetispeed(&tty, B9600);
     cfsetospeed(&tty, B9600);
 
-    tty.c_cflag |= (CLOCAL | CREAD);
-    tty.c_cflag &= ~CSIZE;
-    tty.c_cflag |= CS8;
-    tty.c_cflag &= ~PARENB;
-    tty.c_cflag &= ~CSTOPB;
-    tty.c_cflag &= ~CRTSCTS;
+    tty.c_cflag |= (CLOCAL | CREAD); // local connection, enable reading
+    tty.c_cflag &= ~CSIZE;           // Clear current character size mask
+    tty.c_cflag |= CS8;              // 8 bits per byte
+    tty.c_cflag &= ~PARENB;          // No parity bit
+    tty.c_cflag &= ~CSTOPB;          // 1 stop bit
+    tty.c_cflag &= ~CRTSCTS;         // Disable RTS/CTS hardware flow control, Arduino doesn't use it
 
+    // Apply the configuration to the serial port
     tcsetattr(serial_port, TCSANOW, &tty);
 
     char buffer[256];
@@ -38,7 +43,7 @@ int main() {
     while(true) {
         int n = read(serial_port, &buffer, sizeof(buffer) - 1);
         if(n > 0) {
-            buffer[n] = '\0';
+            buffer[n] = '\0'; // make proper C-string
             std::cout << buffer;
         }
     }
